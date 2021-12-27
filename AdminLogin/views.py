a5d92a7409a788fa463a3fcc35450289683546b7
Pritthika Dhar <Pritthika.dhar@northsouth.edu>
2021-12-27 09:05:22 +0600

    Admin Login Dashboard Created

---
+++ AdminLogin/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,Group
+from .models import *
+from django.contrib.auth import authenticate,logout,login
+from django.utils import timezone
+
+# Create your views here.
+
+def homepage(request):
+	return render(request,'index.html')
+
+
+def Login_admin(request):
+	error = ""
+	if request.method == 'POST':
+		u = request.POST['username']
+		p = request.POST['password']
+		user = authenticate(username=u,password=p)
+		try:
+			if user.is_staff:
+				login(request,user)
+				error = "no"
+			else:
+				error = "yes"
+		except:
+			error = "yes"
+	d = {'error' : error}
+	return render(request,'adminlogin.html',d)
+
+
+
+
+def Logout(request):
+	if not request.user.is_active:
+		return redirect('loginpage')
+	logout(request)
+	return redirect('loginpage')
+
+def Logout_admin(request):
+	if not request.user.is_staff:
+		return redirect('login_admin')
+	logout(request)
+	return redirect('login_admin')
+
